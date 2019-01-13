8d9af6f1e60619127953339a291b143c93c34027
Piszkos12 <piszkos12@gmail.com>
2019-01-13 09:45:05 +0100

    Initial commit

---
+++ LaserEngraver/LaserEngraver.ino
@@ -0,0 +1,32 @@
+/*
+ *  3D laser engraver with
+ *    1 × ESP8266 Wemos D1 Mini Pro hardware version 1.1
+ *    2 × 28BYJ-48 step motor
+ *    2 × ULN2003 driver board  *    
+ *  
+ *  Units: 
+ *          Laser driver: Beatrix
+ *  X-Axis stepper motor: Alice
+ *  Y-Axis stepper motor: Bob
+ *  
+ *  Beatrix
+ *  [PowerOn] -> D0 : GPIO16
+ *  
+ *  Alice
+ *        [0] -> D5 : GPIO14
+ *        [1] -> D6 : GPIO12
+ *        [2] -> D7 : GPIO13
+ *        [3] -> D8 : GPIO15
+ *  
+ *  Bob
+ *        [0] -> D1 : GPIO05 (i2c unfortunately missed)
+ *        [1] -> D2 : GPIO04 (i2c unfortunately missed)
+ *        [2] -> D3 : GPIO00
+ *        [3] -> D4 : GPIO02
+ */
+ 
+void setup() {
+}
+
+void loop() {
+}
